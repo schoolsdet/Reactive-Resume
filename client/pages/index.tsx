647a9fb656e4ad7c0b11e724c57dc7981232d080
Baurz <baurzhan.zh@gmail.com>
2023-02-11 18:26:13 -0500

    Comment stuff on title page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,26 +1,26 @@
-import { DarkMode, LightMode, Link as LinkIcon } from '@mui/icons-material';
-import { Masonry } from '@mui/lab';
+import { DarkMode, LightMode } from '@mui/icons-material';
+// import { Masonry } from '@mui/lab';
 import { Button, IconButton, NoSsr } from '@mui/material';
 import type { GetStaticProps, NextPage } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Trans, useTranslation } from 'next-i18next';
+import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-import Testimony from '@/components/landing/Testimony';
+// import Testimony from '@/components/landing/Testimony';
 import Footer from '@/components/shared/Footer';
 import LanguageSwitcher from '@/components/shared/LanguageSwitcher';
 import Logo from '@/components/shared/Logo';
 import { screenshots } from '@/config/screenshots';
 import { FLAG_DISABLE_SIGNUPS } from '@/constants/flags';
-import testimonials from '@/data/testimonials';
+// import testimonials from '@/data/testimonials';
 import { logout } from '@/store/auth/authSlice';
 import { setTheme } from '@/store/build/buildSlice';
 import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import { setModalState } from '@/store/modal/modalSlice';
 import styles from '@/styles/pages/Home.module.scss';
 
-import { DIGITALOCEAN_URL, DOCS_URL, DONATION_URL, GITHUB_URL, REDDIT_URL } from '../constants';
+// import { DIGITALOCEAN_URL, DOCS_URL, DONATION_URL, GITHUB_URL, REDDIT_URL } from '../constants';
 
 export const getStaticProps: GetStaticProps = async ({ locale = 'en' }) => ({
   props: {
@@ -124,84 +124,84 @@ const Home: NextPage = () => {
         </div>
       </section>
 
-      <section className={styles.section}>
-        <h6>{t<string>('landing.testimonials.heading')}</h6>
+      {/*<section className={styles.section}>*/}
+      {/*  <h6>{t<string>('landing.testimonials.heading')}</h6>*/}
 
-        <p className="my-3">
-          <Trans t={t} i18nKey="landing.testimonials.body">
-            Good or bad, I would love to hear your opinion on Reactive Resume and how the experience has been for you.
-            <br />
-            Here are some of the messages sent in by users across the world.
-          </Trans>
-        </p>
+      {/*  <p className="my-3">*/}
+      {/*    <Trans t={t} i18nKey="landing.testimonials.body">*/}
+      {/*      Good or bad, I would love to hear your opinion on Reactive Resume and how the experience has been for you.*/}
+      {/*      <br />*/}
+      {/*      Here are some of the messages sent in by users across the world.*/}
+      {/*    </Trans>*/}
+      {/*  </p>*/}
 
-        <p className="my-3">
-          <Trans t={t} i18nKey="landing.testimonials.contact">
-            You can reach out to me through <a href="mailto:im.amruth@gmail.com">my email</a> or through the contact
-            form on <a href="https://www.amruthpillai.com">my website</a>.
-          </Trans>
-        </p>
+      {/*  <p className="my-3">*/}
+      {/*    <Trans t={t} i18nKey="landing.testimonials.contact">*/}
+      {/*      You can reach out to me through <a href="mailto:im.amruth@gmail.com">my email</a> or through the contact*/}
+      {/*      form on <a href="https://www.amruthpillai.com">my website</a>.*/}
+      {/*    </Trans>*/}
+      {/*  </p>*/}
 
-        <Masonry columns={{ xs: 1, sm: 2, lg: 4 }} spacing={2}>
-          {testimonials.map(({ name, message }, index) => (
-            <Testimony key={index} name={name} message={message} />
-          ))}
-        </Masonry>
-      </section>
+      {/*  <Masonry columns={{ xs: 1, sm: 2, lg: 4 }} spacing={2}>*/}
+      {/*    {testimonials.map(({ name, message }, index) => (*/}
+      {/*      <Testimony key={index} name={name} message={message} />*/}
+      {/*    ))}*/}
+      {/*  </Masonry>*/}
+      {/*</section>*/}
 
-      <section className={styles.section}>
-        <h6>{t<string>('landing.links.heading')}</h6>
+      {/*<section className={styles.section}>*/}
+      {/*  <h6>{t<string>('landing.links.heading')}</h6>*/}
 
-        <div>
-          <Link href="/meta/privacy" passHref>
-            <Button variant="text" startIcon={<LinkIcon />}>
-              {t<string>('landing.links.links.privacy')}
-            </Button>
-          </Link>
+      {/*  <div>*/}
+      {/*    <Link href="/meta/privacy" passHref>*/}
+      {/*      <Button variant="text" startIcon={<LinkIcon />}>*/}
+      {/*        {t<string>('landing.links.links.privacy')}*/}
+      {/*      </Button>*/}
+      {/*    </Link>*/}
 
-          <Link href="/meta/service" passHref>
-            <Button variant="text" startIcon={<LinkIcon />}>
-              {t<string>('landing.links.links.service')}
-            </Button>
-          </Link>
+      {/*    <Link href="/meta/service" passHref>*/}
+      {/*      <Button variant="text" startIcon={<LinkIcon />}>*/}
+      {/*        {t<string>('landing.links.links.service')}*/}
+      {/*      </Button>*/}
+      {/*    </Link>*/}
 
-          <a href={GITHUB_URL} target="_blank" rel="noreferrer">
-            <Button variant="text" startIcon={<LinkIcon />}>
-              {t<string>('landing.links.links.github')}
-            </Button>
-          </a>
+      {/*    <a href={GITHUB_URL} target="_blank" rel="noreferrer">*/}
+      {/*      <Button variant="text" startIcon={<LinkIcon />}>*/}
+      {/*        {t<string>('landing.links.links.github')}*/}
+      {/*      </Button>*/}
+      {/*    </a>*/}
 
-          <a href={DOCS_URL} target="_blank" rel="noreferrer">
-            <Button variant="text" startIcon={<LinkIcon />}>
-              {t<string>('landing.links.links.docs')}
-            </Button>
-          </a>
+      {/*    <a href={DOCS_URL} target="_blank" rel="noreferrer">*/}
+      {/*      <Button variant="text" startIcon={<LinkIcon />}>*/}
+      {/*        {t<string>('landing.links.links.docs')}*/}
+      {/*      </Button>*/}
+      {/*    </a>*/}
 
-          <a href={REDDIT_URL} target="_blank" rel="noreferrer">
-            <Button variant="text" startIcon={<LinkIcon />}>
-              {t<string>('landing.links.links.reddit')}
-            </Button>
-          </a>
+      {/*    <a href={REDDIT_URL} target="_blank" rel="noreferrer">*/}
+      {/*      <Button variant="text" startIcon={<LinkIcon />}>*/}
+      {/*        {t<string>('landing.links.links.reddit')}*/}
+      {/*      </Button>*/}
+      {/*    </a>*/}
 
-          <a href={DONATION_URL} target="_blank" rel="noreferrer">
-            <Button variant="text" startIcon={<LinkIcon />}>
-              {t<string>('landing.links.links.donate')}
-            </Button>
-          </a>
-        </div>
-      </section>
+      {/*    <a href={DONATION_URL} target="_blank" rel="noreferrer">*/}
+      {/*      <Button variant="text" startIcon={<LinkIcon />}>*/}
+      {/*        {t<string>('landing.links.links.donate')}*/}
+      {/*      </Button>*/}
+      {/*    </a>*/}
+      {/*  </div>*/}
+      {/*</section>*/}
 
-      <section className={styles.section}>
-        <a href={DIGITALOCEAN_URL} target="_blank" rel="noreferrer">
-          <Image
-            src={`/images/sponsors/${theme == 'dark' ? 'digitalocean' : 'digitaloceanLight'}.svg`}
-            style={{ width: 200, height: 40, objectFit: 'contain' }}
-            alt="Powered By DigitalOcean"
-            width={200}
-            height={40}
-          />
-        </a>
-      </section>
+      {/*<section className={styles.section}>*/}
+      {/*  <a href={DIGITALOCEAN_URL} target="_blank" rel="noreferrer">*/}
+      {/*    <Image*/}
+      {/*      src={`/images/sponsors/${theme == 'dark' ? 'digitalocean' : 'digitaloceanLight'}.svg`}*/}
+      {/*      style={{ width: 200, height: 40, objectFit: 'contain' }}*/}
+      {/*      alt="Powered By DigitalOcean"*/}
+      {/*      width={200}*/}
+      {/*      height={40}*/}
+      {/*    />*/}
+      {/*  </a>*/}
+      {/*</section>*/}
 
       <footer>
         <div className={styles.version}>
