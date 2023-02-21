3d58fafd2255e3b4dfda7191bd5ca68c3a1eecbc
nurgitlab <nurbek.math@gmail.com>
2023-02-21 22:32:58 +0300

    removed: empty function

--- client/components/build/LeftSidebar/sections/Basics.tsx
+++ client/components/build/LeftSidebar/sections/Basics.tsx
@@ -1,4 +1,4 @@
-import { Button, Divider, Popover } from '@mui/material';
+import { Divider, Popover } from '@mui/material';
 import { useTranslation } from 'next-i18next';
 import { useState } from 'react';
 
@@ -12,10 +12,6 @@ const Basics = () => {
   const { t } = useTranslation();
 
   const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
-
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
 
   const handleClose = () => {
     setAnchorEl(null);
